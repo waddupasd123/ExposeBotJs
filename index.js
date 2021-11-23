0dfc4d712c90f42b8e8eb3954ca79404751eef2e
Andrew Chen <z5259171@ad.unsw.edu.au>
2021-11-23 13:52:19 +1100

    Initial setup done

---
+++ index.js
@@ -0,0 +1,56 @@
+// Require the necessary discord.js classes
+require("dotenv").config();
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = process.env.DISCORD_TOKEN;
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const PREFIX = "ex ";
+
+client.commands = new Collection();
+
+// Events
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+// Commands
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+// Login to Discord with your client's token
+client.login(token);
