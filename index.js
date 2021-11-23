00c0afed96f27abb710efc3729ab330f4fff7052
Andrew Chen <z5259171@ad.unsw.edu.au>
2021-11-23 14:04:13 +1100

    Added a line to see if heroku works

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Require the necessary discord.js classes
-require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = process.env.DISCORD_TOKEN;
