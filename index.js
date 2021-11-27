eb30c95489e1ffee1509e9a98e09760826c2def8
Andrew Chen <z5259171@ad.unsw.edu.au>
2021-11-27 20:14:23 +1100

    Installed ffmpeg-static

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ const guildId = process.env.GUILD_ID;
 const client = new Client({ 
 	intents: [
 		Intents.FLAGS.GUILDS, 
+		Intents.FLAGS.GUILD_MEMBERS,
 		Intents.FLAGS.GUILD_MESSAGES,
 		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		Intents.FLAGS.GUILD_VOICE_STATES,
 	] 
 });
 
