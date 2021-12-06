eb5afbfadc375972143041be521763927dc39603
Andrew Chen <z5259171@ad.unsw.edu.au>
2021-12-06 19:36:09 +1100

    Changed timing

--- commands/voice/voice.js
+++ commands/voice/voice.js
@@ -73,7 +73,7 @@ module.exports = {
         }
         
         if (!interaction.client.voiceCheck.get(interaction.client.guildId)) {
-            var timerId = setInterval(check, 600000);
+            var timerId = setInterval(check, 300000);
             interaction.client.voiceCheck.set(interaction.client.guildId, true);
             interaction.client.voiceCheck.set(process.env.TARGET_ID, false);
             await interaction.reply('start talking...');
