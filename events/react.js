5c0e4fe1bd97c4abd99a9b278de9c2ec00b00362
Andrew Chen <andrewchen351@gmail.com>
2024-05-15 15:25:46 +1000

    Fixed unavailable react emojis causing crashes

--- events/react.js
+++ events/react.js
@@ -4,7 +4,7 @@ module.exports = {
         if (message.author.bot || message.channel.type === "dm") return;
 
         if (Math.random() < 0.06) {
-            emojis = message.guild.emojis.cache;
+            emojis = message.guild.emojis.cache.filter(emoji => emoji.available === true);
             message.react(emojis.random().id);
         }
     }
